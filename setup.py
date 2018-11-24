03fa48536388493c7cbe1d3946ef5020ab5f125a
anil kumar <anil2outdo@gmail.com>
2018-11-25 02:20:33 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup, find_packages
+
+
+description = """
+    Simple interface to Raspberry Pi Plantpot Greenhouse add-on board
+"""
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="rpi-greenhouse",
+    version="0.4.1",
+    author="Ben Nuttall",
+    author_email="ben@raspberrypi.org",
+    description=description.strip(),
+    long_description=read('README.rst'),
+    license="BSD",
+    keywords=[
+        "raspberry pi",
+        "greenhouse",
+        "garden",
+        "plant",
+        "sensor",
+    ],
+    url="https://github.com/bennuttall/rpi-greenhouse",
+    packages=find_packages(),
+    scripts=[
+        'scripts/greenhouse-logger',
+        'scripts/greenhouse-indicator',
+        'scripts/greenhouse-clear-db',
+    ],
+    install_requires=[
+        "RPi.GPIO",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2",
+        "Topic :: Home Automation",
+        "Topic :: Education",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
